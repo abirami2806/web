c3fbd475a8ea37e8f33cbdaeb5d7da32be6bc2a1
abirami2806 <79451679+abirami2806@users.noreply.github.com>
2022-06-16 23:00:58 +0530

    Create controllerapp.js

---
+++ controllerapp.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html ng-app="newApp">
+<head>
+    <title>Component Creation</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <script src="controllerapp.js"></script>
+    <script src="component.js"></script>
+</head>
+<body>
+    <new-list></new-list>
+</body>
+</html>
